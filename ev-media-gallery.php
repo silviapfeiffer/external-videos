2f98b4894d10e7bfcef27efa6758b1f826d7feb9
johnfjohnf <johnfjohnf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-28 15:06:01 +0000

    Fix post screen
    git-svn-id: http://plugins.svn.wordpress.org/external-videos/trunk@378418 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ev-media-gallery.php
+++ ev-media-gallery.php
@@ -215,7 +215,7 @@ function wp_edit_external_videos_query( $q = false ) {
     $q['m']   = isset( $q['m'] ) ? (int) $q['m'] : 0;
     $q['cat'] = isset( $q['cat'] ) ? (int) $q['cat'] : 0;
     $q['post_type'] = 'external-videos';
-    $q['post_status'] = isset( $q['status'] ) && 'trash' == $q['status'] ? 'trash' : 'inherit';
+    //$q['post_status'] = isset( $q['status'] ) && 'trash' == $q['status'] ? 'trash' : 'inherit'; This breaks the media attach screen on the post and the videos don't get listed
     $media_per_page = (int) get_user_option( 'upload_per_page' );
     if ( empty( $media_per_page ) || $media_per_page < 1 )
         $media_per_page = 20;
